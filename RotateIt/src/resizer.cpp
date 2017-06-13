3ea1a00166b827e5622f6ee563040341b1068ac0
Vitalii17 <shynkov@bigmir.net>
2017-06-13 23:57:16 +0300

    Changed resizing algorithm

--- RotateIt/src/resizer.cpp
+++ RotateIt/src/resizer.cpp
@@ -1,5 +1,9 @@
 #include "resizer.h"
 
+#include <QImageReader>
+
+#include <QDebug>
+
 Resizer::Resizer(QObject *parent) : QObject(parent)
 {
 
@@ -17,9 +21,56 @@ int Resizer::height() const
 
 void Resizer::process()
 {
-    m_outputImage = QImage(m_path).
-            scaled(width(), height(), Qt::KeepAspectRatio).
-            convertToFormat(QImage::Format_ARGB32_Premultiplied);
+    QImageReader imageReader(m_path);
+
+    QSize originalSize(imageReader.size());
+    QSize scaledSize = originalSize;
+
+    if((originalSize.width() || originalSize.height()) == 0)
+    {
+        return;
+    }
+
+//    qDebug() << "!!!!!!!!!" << originalSize.width() / width() <<
+//                originalSize.height() / height();
+
+    // Source form coefficient
+    double kfs = static_cast<double>(originalSize.width()) /
+                 static_cast<double>(originalSize.height());
+
+    // Destination form coefficient
+    double kfd = static_cast<double>(width()) /
+                 static_cast<double>(height());
+
+    double scaleFactor = 1.0;
+
+    if(kfs > kfd)
+    {
+        scaleFactor = static_cast<double>(width()) /
+                static_cast<double>(originalSize.width());
+        scaledSize.setWidth(width());
+        scaledSize.setHeight(originalSize.height() * scaleFactor);
+        //qDebug() << "kfs > kfd";
+    }
+    else
+    {
+        scaleFactor = static_cast<double>(height()) /
+                static_cast<double>(originalSize.height());
+        scaledSize.setWidth(originalSize.width() * scaleFactor);
+        scaledSize.setHeight(height());
+        //qDebug() << "kfs < kfd";
+    }
+
+//    qDebug() << "Original size: " << originalSize;
+//    qDebug() << "Scaled size: " << scaledSize;
+//    qDebug() << "kfs =" << kfs;
+//    qDebug() << "kfd =" << kfd;
+
+    imageReader.setScaledSize(scaledSize);
+    imageReader.read(&m_outputImage);
+
+    m_outputImage = m_outputImage.convertToFormat(QImage::Format_ARGB32_Premultiplied);
+
     emit finished();
     emit finished(m_outputImage);
 }
