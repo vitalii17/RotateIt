924f0158cfbbc1d6723213961aa736c806e3077c
Vitalii17 <shynkov@bigmir.net>
2018-09-08 19:46:44 +0300

    Updated translations

--- RotateIt/translations/Russian.ts
+++ RotateIt/translations/Russian.ts
@@ -10,8 +10,43 @@
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="35"/>
+        <source>&lt;p&gt;Fix your photos directly on your smartphone. Correct the &quot;littered horizon&quot; has never been so easy! &lt;p&gt;Developer: &lt;a href=&quot;http://m.vk.com/vitalis17&quot;&gt;Vitalii Shunkov&lt;/a&gt; (mail: Shynkov@bigmir.net) &lt;p&gt;Special thanks: &lt;a href=&quot;http://m.vk.com/symbian_zone&quot;&gt;&quot;Symbian Zone&quot;&lt;/a&gt; and &lt;a href=&quot;http://forum.dailymobile.net&quot;&gt;&quot;dailymobile.net&quot;&lt;/a&gt; &lt;p&gt;Source code available on &lt;a href=&quot;https://github.com/Vitalii17/RotateIt&quot;&gt;&quot;GitHub.com&quot;&lt;/a&gt; &lt;h3&gt;&lt;p align = &quot;center&quot;&gt;About extended(unsigned) version&lt;/h3&gt; &lt;p&gt;Opening images from Gallery available only in unsigned version. You can download extended version on AppList or on &lt;a href=&quot;https://github.com/Vitalii17/RotateIt/releases&quot;&gt;&quot;GitHub&quot;&lt;/a&gt;. Note that your Symbian phone needs to be hacked and an InstallServer patch applied to ROMPatcher. Most Custom Firmwares (CFW) already have a modified InstallServer to allow installing unsigned apps. &lt;h3&gt;&lt;p align = &quot;center&quot;&gt;License&lt;/h3&gt; &lt;h4&gt;&lt;p align = &quot;center&quot;&gt;The MIT License (MIT)&lt;/h4&gt; &lt;p alifn = &quot;center&quot;&gt;Copyright (c) 2017 Vitalii Shunkov &lt;p&gt;Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the &quot;Software&quot;), to deal in the Software with out restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:&lt;p&gt;The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.&lt;p&gt;THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</source>
+        <translation>&lt;p&gt;Исправляйте Ваши фотографии прямо на смартфоне. Поправить &quot;заваленный горизонт&quot; никогда 
+не было так просто!
+&lt;p&gt;Разработчик: &lt;a href=&quot;http://m.vk.com/vitalis17&quot;&gt;Виталий Шунков&lt;/a&gt; 
+(mail: Shynkov@bigmir.net)
+&lt;p&gt;Благодарности: &lt;a href=&quot;http://m.vk.com/symbian_zone&quot;&gt;&quot;Symbian Zone&quot;&lt;/a&gt; и 
+&lt;a href=&quot;http://forum.dailymobile.net&quot;&gt;&quot;dailymobile.net&quot;&lt;/a&gt;
+&lt;p&gt;Исходный код доступен на 
+&lt;a href=&quot;https://github.com/Vitalii17/RotateIt&quot;&gt;&quot;GitHub.com&quot;&lt;/a&gt; 
+&lt;h3&gt;&lt;p align = \&quot;center\&quot;&gt;О расширенной (неподписанной) версии&lt;/h3&gt; 
+&lt;p&gt;Выбор изображений из стандартного приложения &quot;Галерея&quot; на Symbian доступен только в 
+неподписанной версии. Вы можете загрузить расширенную версию в Applist или на 
+&lt;a href=\&quot;https://github.com/Vitalii17/RotateIt/releases\&quot;&gt;&quot;GitHub&quot;&lt;/a&gt;. Для установки 
+неподписанной версии ваш смартфон должен быть взломан. Большинство модифицированных прошивок 
+для Symbian уже имеют взлом. 
+&lt;h3&gt;&lt;p align = &quot;center&quot;&gt;Информация о лицензии&lt;/h3&gt;
+&lt;h4&gt;&lt;p align = &quot;center&quot;&gt;Лицензия MIT (The MIT License)&lt;/h4&gt;
+&lt;p alifn = &quot;center&quot;&gt;Copyright (c) 2017 Виталий Шунков
+&lt;p&gt;Данная лицензия разрешает лицам, получившим копию данного программного обеспечения и 
+сопутствующей документации (в дальнейшем именуемыми &quot;Программное Обеспечение&quot;), безвозмездно 
+использовать Программное Обеспечение без ограничений, включая неограниченное право на 
+использование, копирование, изменение, слияние, публикацию, распространение, 
+сублицензирование и/или продажу копий Программного Обеспечения, а также лицам, которым 
+предоставляется данное Программное Обеспечение, при соблюдении следующих условий:
+&lt;p&gt;Указанное выше уведомление об авторском праве и данные условия должны быть включены 
+во все копии или значимые части данного Программного Обеспечения.
+&lt;p&gt;ДАННОЕ ПРОГРАММНОЕ ОБЕСПЕЧЕНИЕ ПРЕДОСТАВЛЯЕТСЯ &quot;КАК ЕСТЬ&quot;, БЕЗ КАКИХ-ЛИБО ГАРАНТИЙ, 
+ЯВНО ВЫРАЖЕННЫХ ИЛИ ПОДРАЗУМЕВАЕМЫХ, ВКЛЮЧАЯ ГАРАНТИИ ТОВАРНОЙ ПРИГОДНОСТИ, СООТВЕТСТВИЯ 
+ПО ЕГО КОНКРЕТНОМУ НАЗНАЧЕНИЮ И ОТСУТСТВИЯ НАРУШЕНИЙ, НО НЕ ОГРАНИЧИВАЯСЬ ИМИ. НИ В КАКОМ 
+СЛУЧАЕ АВТОРЫ ИЛИ ПРАВООБЛАДАТЕЛИ НЕ НЕСУТ ОТВЕТСТВЕННОСТИ ПО КАКИМ-ЛИБО ИСКАМ, ЗА УЩЕРБ 
+ИЛИ ПО ИНЫМ ТРЕБОВАНИЯМ, В ТОМ ЧИСЛЕ, ПРИ ДЕЙСТВИИ КОНТРАКТА, ДЕЛИКТЕ ИЛИ ИНОЙ СИТУАЦИИ, 
+ВОЗНИКШИМ ИЗ-ЗА ИСПОЛЬЗОВАНИЯ ПРОГРАММНОГО ОБЕСПЕЧЕНИЯ ИЛИ ИНЫХ ДЕЙСТВИЙ С ПРОГРАММНЫМ 
+ОБЕСПЕЧЕНИЕМ.</translation>
+    </message>
+    <message>
         <source>&lt;p&gt;Fix your photos directly on your smartphone. Correct the &quot;littered horizon&quot; has never been so easy! &lt;p&gt;Developer: &lt;a href=&quot;http://m.vk.com/vitalis17&quot;&gt;Vitalii Shunkov&lt;/a&gt; (mail: Shynkov@bigmir.net) &lt;p&gt;Special thanks: &lt;a href=&quot;http://m.vk.com/symbian_zone&quot;&gt;&quot;Symbian Zone&quot;&lt;/a&gt; and &lt;a href=&quot;http://forum.dailymobile.net&quot;&gt;&quot;dailymobile.net&quot;&lt;/a&gt; &lt;p&gt;Source code available on &lt;a href=&quot;https://github.com/Vitalii17/RotateIt&quot;&gt;&quot;GitHub.com&quot;&lt;/a&gt; &lt;h3&gt;&lt;p align = &quot;center&quot;&gt;License&lt;/h3&gt; &lt;h4&gt;&lt;p align = &quot;center&quot;&gt;The MIT License (MIT)&lt;/h4&gt; &lt;p alifn = &quot;center&quot;&gt;Copyright (c) 2017 Vitalii Shunkov &lt;p&gt;Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the &quot;Software&quot;), to deal in the Software with out restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:&lt;p&gt;The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.&lt;p&gt;THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</source>
-        <translation>&lt;p&gt;Исправляйте Ваши фотографии прямо на смартфоне. Поправить &quot;заваленный горизонт&quot; никогда не было так просто!
+        <translation type="obsolete">&lt;p&gt;Исправляйте Ваши фотографии прямо на смартфоне. Поправить &quot;заваленный горизонт&quot; никогда не было так просто!
 &lt;p&gt;Разработчик: &lt;a href=&quot;http://m.vk.com/vitalis17&quot;&gt;Виталий Шунков&lt;/a&gt; 
 (mail: Shynkov@bigmir.net)
 &lt;p&gt;Благодарности: &lt;a href=&quot;http://m.vk.com/symbian_zone&quot;&gt;&quot;Symbian Zone&quot;&lt;/a&gt; и 
@@ -24,7 +59,7 @@
 &lt;p&gt;Данная лицензия разрешает лицам, получившим копию данного программного обеспечения и сопутствующей документации (в дальнейшем именуемыми &quot;Программное Обеспечение&quot;), безвозмездно использовать Программное Обеспечение без ограничений, включая неограниченное право на использование, копирование, изменение, слияние, публикацию, распространение, сублицензирование и/или продажу копий Программного Обеспечения, а также лицам, которым предоставляется данное Программное Обеспечение, при соблюдении следующих условий:&lt;p&gt;Указанное выше уведомление об авторском праве и данные условия должны быть включены во все копии или значимые части данного Программного Обеспечения.&lt;p&gt;ДАННОЕ ПРОГРАММНОЕ ОБЕСПЕЧЕНИЕ ПРЕДОСТАВЛЯЕТСЯ &quot;КАК ЕСТЬ&quot;, БЕЗ КАКИХ-ЛИБО ГАРАНТИЙ, ЯВНО ВЫРАЖЕННЫХ ИЛИ ПОДРАЗУМЕВАЕМЫХ, ВКЛЮЧАЯ ГАРАНТИИ ТОВАРНОЙ ПРИГОДНОСТИ, СООТВЕТСТВИЯ ПО ЕГО КОНКРЕТНОМУ НАЗНАЧЕНИЮ И ОТСУТСТВИЯ НАРУШЕНИЙ, НО НЕ ОГРАНИЧИВАЯСЬ ИМИ. НИ В КАКОМ СЛУЧАЕ АВТОРЫ ИЛИ ПРАВООБЛАДАТЕЛИ НЕ НЕСУТ ОТВЕТСТВЕННОСТИ ПО КАКИМ-ЛИБО ИСКАМ, ЗА УЩЕРБ ИЛИ ПО ИНЫМ ТРЕБОВАНИЯМ, В ТОМ ЧИСЛЕ, ПРИ ДЕЙСТВИИ КОНТРАКТА, ДЕЛИКТЕ ИЛИ ИНОЙ СИТУАЦИИ, ВОЗНИКШИМ ИЗ-ЗА ИСПОЛЬЗОВАНИЯ ПРОГРАММНОГО ОБЕСПЕЧЕНИЯ ИЛИ ИНЫХ ДЕЙСТВИЙ С ПРОГРАММНЫМ ОБЕСПЕЧЕНИЕМ.</translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="80"/>
+        <location filename="../qml/AboutPage.qml" line="86"/>
         <source>Back</source>
         <translation>Назад</translation>
     </message>
@@ -126,119 +161,123 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="161"/>
+        <location filename="../qml/main.qml" line="162"/>
         <source>Exit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="174"/>
-        <location filename="../qml/main.qml" line="322"/>
+        <location filename="../qml/main.qml" line="175"/>
+        <location filename="../qml/main.qml" line="333"/>
         <source>Open</source>
         <translation>Открыть</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="188"/>
+        <location filename="../qml/main.qml" line="189"/>
         <source>Tools</source>
         <translation>Инструменты</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="207"/>
-        <location filename="../qml/main.qml" line="334"/>
+        <location filename="../qml/main.qml" line="208"/>
+        <location filename="../qml/main.qml" line="345"/>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="217"/>
+        <location filename="../qml/main.qml" line="218"/>
         <source>Menu</source>
         <translation>Меню</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="239"/>
+        <location filename="../qml/main.qml" line="244"/>
         <source>Processing</source>
         <translation>Обработка</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="263"/>
+        <location filename="../qml/main.qml" line="274"/>
         <source>Rotate -90</source>
         <translation>Поворот -90</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="268"/>
+        <location filename="../qml/main.qml" line="279"/>
         <source>Refresh</source>
         <translation>Сброс</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="273"/>
+        <location filename="../qml/main.qml" line="284"/>
         <source>Rotate 90</source>
         <translation>Поворот 90</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="278"/>
+        <location filename="../qml/main.qml" line="289"/>
         <source>Line</source>
         <translation>Горизонт</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="283"/>
+        <location filename="../qml/main.qml" line="294"/>
         <source>View gallery</source>
         <translation>Галерея</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="293"/>
+        <location filename="../qml/main.qml" line="304"/>
         <source>File</source>
         <translation>Файл</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="298"/>
+        <location filename="../qml/main.qml" line="309"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="307"/>
+        <location filename="../qml/main.qml" line="318"/>
         <source>About</source>
         <translation>О Программе</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="346"/>
+        <location filename="../qml/main.qml" line="357"/>
         <source>Image does not saved</source>
         <translation>Изображение не сохранено</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="347"/>
+        <location filename="../qml/main.qml" line="358"/>
         <source>Do you want to save the modified image?</source>
         <translation>Желаете сохранить изображение?</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="348"/>
+        <location filename="../qml/main.qml" line="359"/>
         <source>Yes</source>
         <translation>Да</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="349"/>
+        <location filename="../qml/main.qml" line="360"/>
         <source>No</source>
         <translation>Нет</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="429"/>
+        <location filename="../qml/main.qml" line="439"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="438"/>
+        <location filename="../qml/main.qml" line="448"/>
         <source>From Gallery</source>
         <translation>Из Галереи</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="447"/>
+        <location filename="../qml/main.qml" line="457"/>
         <source>Not available</source>
         <translation>Не доступно</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="448"/>
-        <source>Available only in extended version!</source>
-        <translation>Доступно только в расширенной версии!</translation>
+        <location filename="../qml/main.qml" line="458"/>
+        <source>Available only in extended (unsigned) version! You can download extended version on AppList, GitHub(see &apos;About&apos; for more details) or on Symbian Zone community(vk.com/symbian_zone).  Note that your Symbian phone needs to be hacked and an InstallServer patch applied to ROMPatcher. Most Custom Firmwares (CFW) already have a modified InstallServer to allow installing unsigned apps.</source>
+        <translation>Выбор изображений из стандартного приложения &quot;Галерея&quot; на Symbian доступен только в неподписанной версии. Вы можете загрузить расширенную версию в Applist или на GitHub (github.com/Vitalii17/RotateIt/releases). Для установки неподписанной версии ваш смартфон должен быть взломан. Большинство модифицированных прошивок для Symbian уже имеют взлом.</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="454"/>
+        <source>Available only in extended version!</source>
+        <translation type="obsolete">Доступно только в расширенной версии!</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="469"/>
         <source>File Manager</source>
         <translation>Файловый Менеджер</translation>
     </message>
