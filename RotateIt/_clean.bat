28f089a35e69f76198f3c24dd014ef613004e19f
Vitalii17 <shynkov@bigmir.net>
2018-09-08 19:43:18 +0300

    Added scripts for clean Symbian uild directory

---
+++ RotateIt/_clean.bat
@@ -0,0 +1,27 @@
+del .make.cache
+del ABLD.BAT
+del bld.inf
+del Makefile
+del Makefile*.mk
+del *.loc
+del *.rss
+del *.mmp
+del *.pkg
+
+del moc\tmp\ /q
+del moc\ /q
+rmdir moc\tmp\
+rmdir moc\
+
+del obj\ /q
+rmdir obj\
+
+del src\tmp\ /q
+rmdir src\tmp\
+
+del ui\tmp\ /q
+rmdir ui\tmp\
+rmdir ui\
+
+del rcc\ /q
+rmdir rcc\
